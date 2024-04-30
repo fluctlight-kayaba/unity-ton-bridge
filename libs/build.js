aad3ef93d952ca7cd2e586337397796357404a31
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-01 04:09:06 +0700

    bridge (js) build script

---
+++ libs/build.js
@@ -0,0 +1,43 @@
+const { writeFile } = require('fs/promises');
+const { build } = require('esbuild');
+const { watch } = require('chokidar');
+
+const domainUrl = 'http://localhost:8080';
+const isWatchMode = process.argv.includes('--watch');
+
+const buildTonSDK = async () => {
+	try {
+		const connectManifest = {
+			url: `${domainUrl}`,
+			name: 'Ton Game',
+			iconUrl: `${domainUrl}/TemplateData/icon.png`,
+			termsOfUseUrl: `${domainUrl}/term-of-use`,
+			privacyPolicyUrl: `${domainUrl}/privacy-policy`,
+		};
+
+		await build({
+			entryPoints: ['./libs/ton-sdk/index.ts'],
+			bundle: true,
+			outfile: './build/game/ton-sdk.js',
+		});
+
+		await writeFile(
+			'./build/game/tonconnect-manifest.json',
+			JSON.stringify(connectManifest, null, 4),
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+if (isWatchMode) {
+	watch('./libs', {
+		ignoreInitial: true,
+		ignored: ['**/node_modules/**/*', '**/.git/**/*', '**/.idea/**/*'],
+	}).on('all', async () => {
+		await buildTonSDK();
+		console.log('re-build completed...');
+	});
+}
+
+buildTonSDK();
