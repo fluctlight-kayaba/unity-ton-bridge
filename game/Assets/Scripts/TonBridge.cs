6a822875f9ed5e253ebb1b458ba96bacb039aab1
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-01 04:06:48 +0700

    bare minial TonBridge with wallet-life-cycle methods

--- game/Assets/Scripts/TonBridge.cs
+++ game/Assets/Scripts/TonBridge.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TonBridge : MonoBehaviour {
-    void Start() {}
+    public GameObject purchaseForm;
+    private int amount = 0;
 
-    void Update() {}
+    public void SetPurchaseAmount(string amount) {
+        if (int.TryParse(amount, out int parsedAmount)) {
+            this.amount = parsedAmount;
+        } else {
+            Debug.Log("Could not parse amount: " + amount);
+        }
+    }
 
-    public void ExternalCall() {
-        Debug.Log("ExternalCall");
-        Application.ExternalCall("myJavascriptFunc");
+    public void OnWalletConnected(string address) {
+        Debug.Log("A wallet have been connected!");
+        Debug.Log(address);
+        this.purchaseForm.SetActive(true);
+    }
+
+    public void OnWalletDisconnected() {
+        Debug.Log("Wallet have been disconnected");
+        this.purchaseForm.SetActive(false);
+    }
+
+    public void ConnectWallet() { Application.ExternalCall("tsdkConnect"); }
+
+    public void DisconnectWallet() {
+        Application.ExternalCall("tsdkDisconnect");
+    }
+
+    public void PurchaseGameToken(int amount) {
+        Application.ExternalCall("tsdkPurchaseGameToken", this.amount);
     }
 }
